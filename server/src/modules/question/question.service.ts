528b198ccba802d47df1b23abb593f4991726531
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2025-12-11 10:57:22 +0300

    feat:work on the question service all CRUD operation

---
+++ server/src/modules/question/question.service.ts
@@ -0,0 +1,49 @@
+import { id, tr } from "zod/v4/locales";
+import { prisma } from "../../config/db";
+export const createQuestion = async (userId: number, data: any) => {
+  return await prisma.question.create({
+    data: {
+      title: data.title,
+      description: data.description,
+      userId,
+    },
+  });
+};
+export const getAllQuestion = async () => {
+  return await prisma.question.findMany({
+    orderBy: { createdAt: "desc" },
+    include: {
+      user: { select: { username: true } },
+    },
+  });
+};
+export const getQuestionId = async (id: number) => {
+  const question = await prisma.question.findUnique({
+    where: { id },
+    include: {
+      user: { select: { username: true } },
+      answers: {
+        orderBy: { createdAt: "desc" },
+        include: {
+          user: { select: { username: true } },
+        },
+      },
+    },
+  });
+  return question;
+};
+export const updateQuestion = async (id: number, data: any) => {
+  const updateQuestion = await prisma.question.update({
+    where: { id },
+    data: {
+      title: data.title,
+      description: data.description,
+    },
+  });
+  return updateQuestion
+};
+export const deleteQuestion=async (id:number)=>{
+  return  await prisma.question.delete({
+    where:{id}
+  })
+}
