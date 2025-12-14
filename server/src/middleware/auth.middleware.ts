95967658ac9ad548a2b3e5389113ae4abbb816ab
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2025-12-14 10:59:57 +0300

    fix:the middleware when it is split

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -14,9 +14,9 @@ export const authMiddleware = (
     if (!authHeader || !authHeader.startsWith("Bearer")) {
       return res.status(401).json({ message: "No token provided" });
     }
-  const token = authHeader.split('  ')[1];
+  const token = authHeader.split(' ')[1];
     console.log('token: ',token)
-    console.log(authHeader.split('  ')[0])
+    // console.log(authHeader.split('  ')[0])
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
       id: string;
       email: string;
