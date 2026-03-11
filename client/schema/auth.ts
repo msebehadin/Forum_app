ba5d590ae01a5d9a428c9ba22beec853f72f70f3
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2026-03-12 00:35:02 +0300

    fix: the auth js

--- client/schema/auth.ts
+++ client/schema/auth.ts
@@ -5,12 +5,15 @@ export const loginSchema = z.object({
   email: z.string().email("Invalid email").min(1, "Email is required"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
-export const registerSchema=z.object({
-username:z.string().min(1,'username is required'),
-firstName:z.string().min(5,'first name is required'),
-lastName:z.string().min(5,'last name is required'),
-email: z.string().email("Invalid email").min(1, "Email is required"),
-password: z.string().min(6, "Password must be at least 6 characters"),
-})
+export const registerSchema = z.object({
+  username: z.string().min(1, "username is required"),
+  firstName: z.string().min(5, "first name is required"),
+  lastName: z.string().min(5, "last name is required"),
+  email: z.string().email("Invalid email").min(1, "Email is required"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  terms: z.boolean().refine((v) => v === true, {
+    message: "You must accept the terms and conditions",
+  }),
+});
 export type LoginInput = z.infer<typeof loginSchema>;
 export type registerInput=z.infer<typeof registerSchema>;
